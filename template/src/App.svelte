73a4c71d825e2aca9c3980ef0669c29dbd04f39a
Kevin Golding <kevin@kgolding.co.uk>
2025-06-25 10:10:58 +0100

    add navbar

--- template/src/App.svelte
+++ template/src/App.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
+  import Router from "svelte-spa-router";
+  import NavBar from "./lib/Navbar.svelte";
   import { LoginForm, auth } from "smcgateway-sv";
-  import Hello from "lib/Hello.svelte"
+  import Hello from "./lib/Hello.svelte";
+
+  const routes = {
+    "/hello": Hello,
+    // Catch all, must be at end
+    "*": Hello,
+  };
 </script>
 
-<div class="container mt-3">
-  {#if auth.user}
-    <Hello></Hello>
-    <button class="btn btn-secondary" onclick={auth.logout}>Logout</button>
-  {:else}
-    <LoginForm></LoginForm>
-  {/if}
-</div>
+{#if auth.user}
+  <NavBar></NavBar>
+  <div class="container-fluid" style="margin-top: 60px;">
+    <Router {routes} />
+  </div>
+{:else}
+  <div class="form-signin">
+    <LoginForm>
+      <h3>Login</h3>
+    </LoginForm>
+  </div>
+{/if}
+
+<style>
+  .form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding-top: 50px;
+    margin: auto;
+  }
+</style>
---
+++ template/src/lib/Navbar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import active from "svelte-spa-router/active";
+    import { auth } from "smcgateway-sv";
+</script>
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#/">Admin</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a
+                        class="nav-link"
+                        use:active
+                        aria-current="page"
+                        href="#/hello">Hello</a
+                    >
+                </li>
+                {#if auth.isAdmin}
+                    <li class="nav-item">
+                        <a
+                            class="nav-link"
+                            use:active
+                            aria-current="page"
+                            href="#/">Admin only</a
+                        >
+                    </li>
+                {/if}
+            </ul>
+            <a class="nav-link" use:active href="#/" onclick={auth.logout}
+                >Logout</a
+            >
+        </div>
+    </div>
+</nav>
